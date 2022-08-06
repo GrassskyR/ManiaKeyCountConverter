569677614a23525c4c6ce09b661010b92d47a23a
GrassskyR <45731618+GrassskyR@users.noreply.github.com>
2022-08-06 14:55:22 +0800

    Bug Fixs
    Fix  wrong converts on Timing Points

--- ManiaKeyCountConverter.py
+++ ManiaKeyCountConverter.py
@@ -16,11 +16,26 @@ def OsuConvert(Map_path):
 
         osu_content = osu_files.read()
 
-        if osu_content.find("Mode: 3") and osu_content.find("CircleSize:7") != -1:  
+        if osu_content.find("Mode: 3") and osu_content.find("CircleSize:7") != -1:
+            index_pos = osu_content.find('[TimingPoints]')
+            end_pos = osu_content.find('\n',index_pos) + 1      #Start index of the Timing info
+            timing_pos = osu_content.find('\n',end_pos)     #End index of the Timing info
+            TimingInfo = osu_content[end_pos:timing_pos]    #获取Timing Info
+
             osu_content = re.sub(regex,'',osu_content,0,re.M)   #这个正则花了我一个多小时
             osu_content = re.sub("CircleSize:7","CircleSize:6",osu_content)     #正则删空
+
+            if(osu_content[end_pos] == '\n'):
+                osu_content = osu_content[:end_pos] + TimingInfo + osu_content[end_pos + 1:]    #防止错误替换Timing info
+
+            osu_basename = os.path.basename(Map_path)
+            osu_curdir = os.path.dirname(Map_path)
+            osu_newname = "7_to_6_" + osu_basename
+            New_MapPath = os.path.join(osu_curdir,osu_newname)
+            print(New_MapPath)
+
             osu_files.close()
-            osu_files = open(Map_path,"w",encoding='UTF-8')
+            osu_files = open(New_MapPath,"w",encoding='UTF-8')
             osu_files.write(osu_content)
             print("转换完成")
         else :
